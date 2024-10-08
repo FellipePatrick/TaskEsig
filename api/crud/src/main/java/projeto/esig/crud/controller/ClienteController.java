c737c4aae797a88c17d1abbae5577d8d7f14a705
Deto <69614422+d3tu@users.noreply.github.com>
2024-10-07 23:37:27 -0300

    Update ClienteController.java

--- api/crud/src/main/java/projeto/esig/crud/controller/ClienteController.java
+++ api/crud/src/main/java/projeto/esig/crud/controller/ClienteController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // @CrossOrigin(origins = "${HOST_URL}")
-// @CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/tarefas/")
 @AllArgsConstructor
